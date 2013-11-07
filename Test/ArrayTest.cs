ca8ad141e5075440bbdb92db48f24f5c87796307
Julian Thatcher <daedalus.raistlin@gmail.com>
2013-11-08 00:23:02 +1100

    New buffer test

--- Test/ArrayTest.cs
+++ Test/ArrayTest.cs
@@ -12,19 +12,33 @@ namespace Test
 	{
 		public ArrayTest()
 		{
-			var buffer = new ArrayBuffer(12);
-			var x = new Int32Array(buffer, 0);
-			// Make another view of last 4 bytes of the buffer
-			var y = new Int32Array(buffer, 4);
-			x[1] = 7;
-			Console.WriteLine("y[0] = {0}", y[0]);
-			assert(y[0] == 7);
-			assert(y[0] == x[1]);
+			/**
+			 * The following code can be run in a (recent) web browser, and should perform the same.
+			 * You'll need this code above the code below, though:
+			 * 
+			 * function assert(y) { if (!y) throw new Exception("assert failed"); }; var Int32 = { MaxValue: 2147483647, MinValue: -2147483648 };
+			 */
+			var buffer = new ArrayBuffer(20);
+			var x32 = new Int32Array(buffer, 0);
+			var y32 = new Int32Array(buffer, 4);
 
-			var z = new Uint8Array(buffer, 0);
-			z[1] = 255;
-			Console.WriteLine("x[0] = {0}", x[0]);
-			assert(x[0] == (7 + (255 << 8)));
+			// Modifying data before the view doesn't affect the other view
+			x32[0] = 42;
+			assert(y32[0] == 0);
+
+			// Modifying data in the view does affect the other
+			x32[1] = 64;
+			assert(y32[0] == 64);
+
+			// Use a Uint8Array to write a 32bit int
+			x32[0] = 0;
+			assert(x32[0] == 0);
+			var z8 = new Uint8Array(buffer, 16);
+			z8[0] = 255;
+			z8[1] = 255;
+			z8[2] = 255;
+			z8[3] = 127;
+			assert(x32[4] == Int32.MaxValue);
 		}
 	}
 }
